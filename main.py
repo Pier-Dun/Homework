b231e4a7c21f82fbccfa0d4896845ad81ff4c081
Pier-Dun <d.kon.0709@gmail.com>
2024-08-31 21:38:38 +0300

    Практическое задание по уроку "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a, b, a == b)
+
+# 4th program
+string = "123.456"
+number = float(string)
+number *= 10
+number = int(number % 10)
+print(number)
