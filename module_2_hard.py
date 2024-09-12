5c19403f62ccffea36ca0e6bc95914c4fde022b3
Pier-Dun <d.kon.0709@gmail.com>
2024-09-12 17:30:14 +0300

    Update module_2_hard.py
    Меньше итераций

--- module_2_hard.py
+++ module_2_hard.py
@@ -4,9 +4,9 @@ import random
 n = random.choice(range(3, 21))
 result = ''
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if n % (i + j) == 0 and i < j:
+for i in range(1, n // 2 + n % 2):
+    for j in range(i+1, n):
+        if n % (i + j) == 0:
             result += str(i) + str(j)
 
 
