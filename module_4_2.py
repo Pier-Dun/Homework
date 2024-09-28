252161b8ac6fd83b61f76c0f2776c45e5e3fe900
Pier-Dun <d.kon.0709@gmail.com>
2024-09-28 17:26:04 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+# inner_function() является локальной функцией test_function(), к ней нельзя обращаться не из её всеобъемлющего или локального пространства имен
+
+test_function()
