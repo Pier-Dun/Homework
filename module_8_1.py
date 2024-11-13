a9dce18f1830301f2649b7c8b421c4020933e3cf
Pier-Dun <d.kon.0709@gmail.com>
2024-11-13 21:17:05 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,10 @@
+def add_everything_up(a, b):
+    try:
+        return f'{(a + b):.3f}'
+    except TypeError as exc:
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
