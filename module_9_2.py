edc93105906117865a05afe795f0c915ba6318e9
Pier-Dun <d.kon.0709@gmail.com>
2024-11-21 22:16:34 +0300

    Add files via upload

---
+++ module_9_2.py
@@ -0,0 +1,9 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(x) for x in first_strings]
+second_result = [(x, y) for x in first_strings for y in second_strings if len(x) == len(y)]
+third_result = {z: len(z) for z in first_strings + second_strings if not len(z) % 2}
+print(first_result)
+print(second_result)
+print(third_result)
