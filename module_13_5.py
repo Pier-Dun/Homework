16f1f7909936aa6a0ff611c4ba76ee20cd82ebdb
Pier-Dun <d.kon.0709@gmail.com>
2025-01-27 22:37:10 +0300

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,56 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button_1 = KeyboardButton(text='Рассчитать')
+button_2 = KeyboardButton(text='Информация')
+kb.add(button_1)
+kb.add(button_2)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+@dp.message_handler(text=['Рассчитать'])
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    await message.answer(10 * float(data['weight']) + 6.25 * float(data['growth']) - 5 * float(data['age']) + 5)
+    await state.finish()
+
+@dp.message_handler()
+async def start(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
